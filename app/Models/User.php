4b1576e50908bb45566f7476968cb218b9c1ae96
GilbertGallanongo <gallanongogilbert@gmail.com>
2026-06-04 09:41:41 +0800

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,13 +40,10 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
     public function isAdmin(): bool
     {
